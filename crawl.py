307ce2f47c83feb8bc1b7cd02b97e8c54eb229cc
Kyeongpil <rudvlf0413@korea.ac.kr>
2019-12-18 13:02:52 +0900

    .

--- crawl.py
+++ crawl.py
@@ -8,7 +8,8 @@ from tqdm import tqdm
 FUNCTION_URL = ''
 MIN_MOVIE_ID = 10001
 MAX_MOVIE_ID = 191500
-PROCESS_NUM = 24
+PROCESS_NUM = 32
+CHUNK_SIZE = 6
 FUNCTION_URL = 'https://asia-east2-graceful-rope-261606.cloudfunctions.net/crawl-function'
 headers = {'Content-Type': 'application/json; charset=utf-8'}
 
@@ -26,7 +27,8 @@ def request_one(code):
 results = []
 movie_codes = list(range(MIN_MOVIE_ID, MAX_MOVIE_ID + 1))
 with Pool(PROCESS_NUM) as p:
-    for result in tqdm(p.imap_unordered(request_one, movie_codes), total=len(movie_codes)):
+    for result in tqdm(p.imap_unordered(request_one, movie_codes, chunksize=CHUNK_SIZE), 
+                    total=len(movie_codes)):
         results.append(result)
 
 results = [r for r in results if r['status']]
